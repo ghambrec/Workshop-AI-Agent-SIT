9ef4f4b427b137dfadd39e3411fcd687e0fccb23
ghambrecht <gabriel.hambrecht@gmail.com>
2026-04-14 15:14:22 +0200

    reactivated langfuse

--- app/services/agent.py
+++ app/services/agent.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from pathlib import Path
-# from langfuse import get_client
+from langfuse import get_client
 from pydantic_ai import Agent
 from app.models.order import Order
 
@@ -14,15 +14,15 @@ PROMPT_DIR = BASE_DIR / "app/prompts"
 SOUL_MD = PROMPT_DIR / "soul.md"
 
 
-# langfuse = get_client()
+langfuse = get_client()
 
-# # Verify connection
-# if langfuse.auth_check():
-#     print("Langfuse client is authenticated and ready!")
-# else:
-#     print("Authentication failed. Please check your credentials and host.")
+# Verify connection
+if langfuse.auth_check():
+    print("Langfuse client is authenticated and ready!")
+else:
+    print("Authentication failed. Please check your credentials and host.")
 
-# Agent.instrument_all()
+Agent.instrument_all()
 
 
 # ---------------------------------------------------------
@@ -40,7 +40,7 @@ soul_prompt = Path(SOUL_MD).read_text(encoding="utf-8")
 order_agent = Agent(
     "vertexai:gemini-2.5-flash",
     output_type=Order,
-    # instrument=True,
-    # tools=[fetch_store_data, place_restock_order, adjust_price, discount_category],
+    instrument=True,
+    # tools=[],
     system_prompt=soul_prompt,
 )
